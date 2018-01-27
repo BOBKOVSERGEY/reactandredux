e20d59f2f37b6d454cae4451248765e6be5437d5
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2018-01-27 11:12:32 +0300

    Промисы

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var bs = require("browser-sync").create();
 
-bs.watch(['peremennyees6/*.*', 'peremennyees6/css/*.*']).on("change", bs.reload);
+bs.watch(['promise/*.*', 'promise/css/*.*']).on("change", bs.reload);
 
 // Start a Browsersync proxy
 bs.init({
-  server: "./peremennyees6"
+  server: "./promise"
 });
